e92b141167479b2e7f1510914e2727714a03108b
jesus raul alarcon ontiveros <thelowlevelcode@gmail.com>
2020-04-04 17:06:10 -0700

    Se quita la semilla de datos

--- DataInfrastructure/Mapping/PostMap.cs
+++ DataInfrastructure/Mapping/PostMap.cs
@@ -12,18 +12,18 @@ namespace webApiTest.DataInfrastruture.Mapping
             builder.HasKey(p=>p.Id);
             builder.Property(p=>p.CreatedAt).ValueGeneratedOnAdd();
             builder.ToTable("post");
-            builder.HasData(
-                new Post { Id=1, Name="Post 1" , CreatedAt = DateTime.Now },
-                new Post { Id=2, Name="Post 2" , CreatedAt = DateTime.Now },
-                new Post { Id=3, Name="Post 3" , CreatedAt = DateTime.Now },
-                new Post { Id=4, Name="Post 4" , CreatedAt = DateTime.Now },
-                new Post { Id=5, Name="Post 5" , CreatedAt = DateTime.Now },
-                new Post { Id=6, Name="Post 6" , CreatedAt = DateTime.Now },
-                new Post { Id=7, Name="Post 7" , CreatedAt = DateTime.Now },
-                new Post { Id=8, Name="Post 8" , CreatedAt = DateTime.Now },
-                new Post { Id=9, Name="Post 9" , CreatedAt = DateTime.Now },
-                new Post { Id=10, Name="Post 10", CreatedAt = DateTime.Now }
-            );
+            // builder.HasData(
+            //     new Post { Id=1, Name="Post 1" , CreatedAt = DateTime.Now },
+            //     new Post { Id=2, Name="Post 2" , CreatedAt = DateTime.Now },
+            //     new Post { Id=3, Name="Post 3" , CreatedAt = DateTime.Now },
+            //     new Post { Id=4, Name="Post 4" , CreatedAt = DateTime.Now },
+            //     new Post { Id=5, Name="Post 5" , CreatedAt = DateTime.Now },
+            //     new Post { Id=6, Name="Post 6" , CreatedAt = DateTime.Now },
+            //     new Post { Id=7, Name="Post 7" , CreatedAt = DateTime.Now },
+            //     new Post { Id=8, Name="Post 8" , CreatedAt = DateTime.Now },
+            //     new Post { Id=9, Name="Post 9" , CreatedAt = DateTime.Now },
+            //     new Post { Id=10, Name="Post 10", CreatedAt = DateTime.Now }
+            // );
         }
     }
 }
